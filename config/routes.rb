45a07bfc08dfa871df847005cf1f5a1a58f757c3
Ricardo Prado <ricardogmprado@gmail.com>
2019-05-27 18:32:51 +0800

    Bookings controller created with index, show and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :campervan do
+  resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:show, :index]
 end
