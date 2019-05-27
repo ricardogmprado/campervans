59730105d6578dedaa242e268f9d6c3bd20604e5
Ricardo Prado <ricardogmprado@gmail.com>
2019-05-27 17:35:03 +0800

    We finished the has_many relations, and created the validations for each model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :rating, presence: true, inclusion: { in: (1..5).to_a }
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  has_many :reviews, through: :bookings
+  has_many :campervans
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /.+\@.+\..+/ }
+  validates :phone_number, presence: true
 end
