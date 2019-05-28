a4add4cf7c91279d00d6849fd2a89cdb697d85be
bernice_boon <bbvoxy@gmail.com>
2019-05-28 14:21:49 +0800

    redirect and view form

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :campervan do
   resources :bookings, only: [:create]
-  end
 
-  resources :bookings, only: [:show, :index]
+  resources :bookings, only: [:show, :index] do
+  resources :reviews, only: [:create]
 end
+end
+
+
